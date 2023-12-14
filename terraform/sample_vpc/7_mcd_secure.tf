b46c2d0fb1cce1c92343464d0cc9383632dbfa4d
David Staudt <dstaudt@cisco.com>
2023-12-13 18:56:13 -0600

    Action test - full

--- terraform/sample_vpc/7_mcd_secure.tf
+++ terraform/sample_vpc/7_mcd_secure.tf
@@ -11,12 +11,3 @@ resource "ciscomcd_spoke_vpc" "mcd_spoke" {
   service_vpc_id = var.mcd_service_vpc_id
   spoke_vpc_id   = aws_vpc.sample_vpc.id
 }
-
-resource "aws_route" "sample_internet_route" {
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id = var.mcd_transit_gateway_id
-  route_table_id     = aws_route_table.sample_route_table.id
-  depends_on = [
-    ciscomcd_spoke_vpc.mcd_spoke
-  ]
-}
