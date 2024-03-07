e66ff69f0db614b8c75f988113e1b0400e37db30
David Staudt <dstaudt@cisco.com>
2024-03-07 16:23:01 -0600

    Fix ssh key name Remove unused provider version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
-    # --- Step 2: Onboard with Cisco Multicloud Defense ---
+    ## --- Step 2: Onboard with Cisco Multicloud Defense ---
     # ciscomcd = {
     #   source = "ciscodevnet/ciscomcd"
-    #   # version = "0.2.4"
     # }
   }
 
@@ -15,7 +14,7 @@ terraform {
   }
 }
 
-# --- Step 2: Onboard with Cisco Multicloud Defense ---
+## --- Step 2: Onboard with Cisco Multicloud Defense ---
 # provider "ciscomcd" {
 #   api_key_file = file("cisco_mcd/mcd_api_key.json")
 # }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,6 +1,6 @@
 aws_availability_zone1             = "us-east-1a"
 aws_availability_zone2             = "us-east-1b"
-aws_ssh_key_pair_name             = "mcd-lab"
+aws_ssh_key_pair_name             = "mcd-workshop"
 mcd_inventory_regions             = ["us-east-1"]
 mcd_deployment_name               = "prod1"
 mcd_controller_aws_account_number = "878511901175" #AMER region
